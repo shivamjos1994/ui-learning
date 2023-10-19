ace06e150078b729cf3a4f46cf83bd105d5a8a05
shivamjos1994 <shivamjos1994@gmail.com>
2023-10-19 12:33:59 +0530

    added comments

--- scroll.js
+++ scroll.js
@@ -1,5 +1,10 @@
+/**
+ * window.onscroll is used, when a user scrolls down and reaches at the end of the page, something should happen.
+ * Here the color changes to green when the user reaches at the bottom of the page.
+ * ()=> defines an arrow function, it's a shorthand for function()
+ */
 window.onscroll = ()=>{
-  if (window.innerHeight + window.scrollY >= document.body.offsetHeight){
+  if (window.innerHeight + window.scrollY >= document.body.offsetHeight){         // it calculates the bottom of the screen
     document.querySelector('body').style.background = "green";
   }
   else{
@@ -8,7 +13,8 @@ window.onscroll = ()=>{
 };
 
 
-document.addEventListener('DOMContentLoaded', function(){
+// after the contents being loaded 
+document.addEventListener('DOMContentLoaded', function(){     
     let div = document.getElementById('div');
       for(let i=1; i<=100; i++){
         let para = document.createElement("p");
